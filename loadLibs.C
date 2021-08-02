7d60541a013e1c5c7153062fc93722ef6fdbf1b8
Everardo Granados Vazquez <granados@minervagpvm02.fnal.gov>
2021-08-02 16:39:12 -0500

    Commit of the MAD ccpi analysis with the new branches

--- loadLibs.C
+++ loadLibs.C
@@ -1,6 +1,7 @@
 #include "TString.h"
 #include "TSystem.h"
 #include "TInterpreter.h"
+#include "Cintex/Cintex.h"
 
 #include <iostream>
 
@@ -23,9 +24,37 @@ void loadLibs()
   // Compile local scripts
   gInterpreter->ExecuteMacro("loadCuts.C");
 
-  //gSystem->CompileMacro("util/plot/GridCanvas.cxx", "k");
+  // Add to include dirs
+  const int kNIncDirs=2;
+  TString incDirs[kNIncDirs] = {
+    TString::Format("%s", gSystem->Getenv("PLOTUTILSROOT")),
+    TString::Format("%s", gSystem->Getenv("UNFOLDUTILSROOT")),
+  };
+
+  for(int i=0; i<kNIncDirs; ++i){
+    //cout << "Adding " << incDirs[i] << endl;
+    gInterpreter->AddIncludePath(incDirs[i]);
+  }
+
+  // Link plotutils, mcreweight, and others
+  const int kNLibs=3;
+  // TODO: Will probably need to add T2KReweight and the neut reweight library to this
+  const char* libs[kNLibs] = {
+    "libplotutils.so",
+    "libCintex.so",
+    "libUnfoldUtils.so"
+  };
+
+  for(int i=0; i<kNLibs; ++i){
+    cout << "Loading " << libs[i] << endl;
+    gSystem->Load(libs[i]);
+  }
+
+  ROOT::Cintex::Cintex::Enable();
+
+  gSystem->CompileMacro("util/plot/GridCanvas.cxx", "k");
   // Long complicated reason to do this because of using TExec to set colour palettes
-  gSystem->CompileMacro("includes/myPlotStyle.h", "k");
+  gSystem->CompileMacro("util/plot/myPlotStyle.h", "k");
 #endif
 }
 
