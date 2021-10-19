5b6f1b51998844366ed198e0f7f4765739058173
Ben Messerly <bamesserly@gmail.com>
2021-10-19 13:58:35 -0500

    Couple updates to setup scripts.

--- setSL7.sh
+++ setSL7.sh
@@ -1,1 +1,5 @@
-source /cvmfs/minerva.opensciencegrid.org/minerva/software_releases/v22r1p1/setup.sh
+source /cvmfs/minerva.opensciencegrid.org/minerva/hep_hpc_products/setups
+setup root v6_10_04d -q e14:prof
+setup cmake v3_7_1
+
+source /cvmfs/minerva.opensciencegrid.org/minerva/setup/setup_minerva_products.sh
