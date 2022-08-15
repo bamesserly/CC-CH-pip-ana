7edbdd5be559d208ada6c38945d0533a8d588686
egranadosvazquez <granadosve2012@licifug.ugto.mx>
2022-08-15 12:58:11 -0500

    Changes to run the analyis with GENIEv2p3p1

--- studies/runCutVariables.C
+++ studies/runCutVariables.C
@@ -110,7 +110,7 @@ void LoopAndFillCutVars(const CCPi::MacroUtil& util, CVUniverse* universe,
 // Main
 //==============================================================================
 void runCutVariables(int signal_definition_int = 0, 
-                     const char* plist = "ME1A") {
+                     const char* plist = "ALL") {
 
   // Macro Utility
   std::string data_file_list = GetPlaylistFile(plist, false);
