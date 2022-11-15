a817de8fbddaf086ee03d8e7d15b0527e5a5c13e
Ben Messerly <bamesserly@gmail.com>
2022-11-14 22:46:39 -0600

    Autoformat.

--- studies/runEffPurTable.C
+++ studies/runEffPurTable.C
@@ -12,19 +12,22 @@
 //==============================================================================
 // Loop and fill
 //==============================================================================
-std::tuple<EventCount, EventCount> FillCounters(const CCPi::MacroUtil& util, CVUniverse* universe,
-                  const EDataMCTruth& type, const EventCount& s, const EventCount& b = EventCount()) {
+std::tuple<EventCount, EventCount> FillCounters(
+    const CCPi::MacroUtil& util, CVUniverse* universe, const EDataMCTruth& type,
+    const EventCount& s, const EventCount& b = EventCount()) {
   EventCount signal = s;
   EventCount bg = b;
   bool is_mc, is_truth;
   Long64_t n_entries;
   SetupLoop(type, util, is_mc, is_truth, n_entries);
-  for(Long64_t i_event=0; i_event < n_entries; ++i_event){
-    if (i_event%500000==0) std::cout << (i_event/1000) << "k " << std::endl;
+  for (Long64_t i_event = 0; i_event < n_entries; ++i_event) {
+    if (i_event % 500000 == 0)
+      std::cout << (i_event / 1000) << "k " << std::endl;
     universe->SetEntry(i_event);
     CCPiEvent event(is_mc, is_truth, util.m_signal_definition, universe);
-    std::tie(signal, bg) = ccpi_event::FillCounters(event, signal, bg); // Does a lot of work
-  } // events
+    std::tie(signal, bg) =
+        ccpi_event::FillCounters(event, signal, bg);  // Does a lot of work
+  }                                                   // events
   std::cout << "*** Done ***\n\n";
   return {signal, bg};
 }
@@ -48,16 +51,19 @@ void runEffPurTable(int signal_definition_int = 0, const char* plist = "ALL") {
   // typdef EventCount map<ECut, double>
   EventCount n_remaining_sig, n_remaining_bg, n_remaining_data;
 
-  std::tie(n_remaining_data, std::ignore) = 
-      FillCounters(util, util.m_data_universe,  kData, n_remaining_data);
+  std::tie(n_remaining_data, std::ignore) =
+      FillCounters(util, util.m_data_universe, kData, n_remaining_data);
 
-  std::tie(n_remaining_sig, n_remaining_bg) = 
-      FillCounters(util, util.m_error_bands.at("cv").at(0), kMC, n_remaining_sig, n_remaining_bg);
+  std::tie(n_remaining_sig, n_remaining_bg) =
+      FillCounters(util, util.m_error_bands.at("cv").at(0), kMC,
+                   n_remaining_sig, n_remaining_bg);
 
-  std::tie(n_remaining_sig, n_remaining_bg) = 
-      FillCounters(util, util.m_error_bands_truth.at("cv").at(0), kTruth, n_remaining_sig, n_remaining_bg);
+  std::tie(n_remaining_sig, n_remaining_bg) =
+      FillCounters(util, util.m_error_bands_truth.at("cv").at(0), kTruth,
+                   n_remaining_sig, n_remaining_bg);
 
-  PrintEffPurTable(n_remaining_sig, n_remaining_bg, n_remaining_data, util.m_data_pot, util.m_mc_pot);
+  PrintEffPurTable(n_remaining_sig, n_remaining_bg, n_remaining_data,
+                   util.m_data_pot, util.m_mc_pot);
 }
 
 #endif
