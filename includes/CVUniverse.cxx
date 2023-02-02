06b2f63a80498b1c045457785fab8e9be42754a2
Ben Messerly <bamesserly@gmail.com>
2023-02-02 15:08:09 -0600

    Separate changes unrelated to this branch.

--- includes/CVUniverse.cxx
+++ includes/CVUniverse.cxx
@@ -142,16 +142,12 @@ double CVUniverse::GetThetamuDeg() const {
 // event-wide
 double CVUniverse::GetEhad() const {
   return GetRecoilEnergy();
-//  return GetCalRecoilEnergy() + GetTrackRecoilEnergy();
+  //return GetCalRecoilEnergy() + GetTrackRecoilEnergy();
 }
 double CVUniverse::GetEnu() const { return GetEmu() + GetEhad(); }
 
 double CVUniverse::GetQ2() const {
   return CalcQ2(GetEnu(), GetEmu(), GetThetamu(), GetPmu());
-}
-
-double CVUniverse::GetQ2GeV() const {
-  return CalcQ2(GetEnu(), GetEmu(), GetThetamu(), GetPmu()) / 1000000;
 }
 
 double CVUniverse::GetWexp() const { return CalcWexp(GetQ2(), GetEhad()); }
@@ -495,10 +491,6 @@ std::vector<double> CVUniverse::GetTpiTrueVec() const {
     ret.push_back(GetTpiTrue(idx));
   }
   return ret;
-}
-
-double CVUniverse::GetQ2GeVTrue() const {
-  return GetQ2True()/1000000;
 }
 
 //==============================
@@ -895,8 +887,8 @@ double CVUniverse::GetWeight() const {
   wgt_rpa = GetRPAWeight();
 
   // MINOS efficiency
-//  if (!m_is_truth && GetBool("isMinosMatchTrack"))
-//    wgt_mueff = GetMinosEfficiencyWeight();
+  //  if (!m_is_truth && GetBool("isMinosMatchTrack"))
+  //    wgt_mueff = GetMinosEfficiencyWeight();
 
   // 2p2h
   wgt_2p2h = GetLowRecoil2p2hWeight();
@@ -939,7 +931,7 @@ double CVUniverse::GetWeight() const {
 
   wgt_geant = GetGeantHadronWeight();
 
-//  std::cout << wgt_genie << " " << wgt_flux << " " <<  wgt_2p2h << " " << wgt_rpa << " " << wgt_lowq2 << " " << wgt_mueff << " " << wgt_anisodd << " " << wgt_michel << " " << wgt_diffractive << " " << wgt_mk << " " << wgt_target << " " << wgt_fsi << " " << wgt_coh << " " << wgt_geant << " " << wgt_sbfit << "\n";
+  //  std::cout << wgt_genie << " " << wgt_flux << " " <<  wgt_2p2h << " " << wgt_rpa << " " << wgt_lowq2 << " " << wgt_mueff << " " << wgt_anisodd << " " << wgt_michel << " " << wgt_diffractive << " " << wgt_mk << " " << wgt_target << " " << wgt_fsi << " " << wgt_coh << " " << wgt_geant << " " << wgt_sbfit << "\n";
 
   return wgt_genie * wgt_flux * wgt_2p2h * wgt_rpa * wgt_lowq2 * wgt_mueff *
          wgt_anisodd * wgt_michel * wgt_diffractive * wgt_mk * wgt_target *
@@ -1153,17 +1145,17 @@ TVector3 CVUniverse::AdlerAngle(int RefSystemDef, double dmumom /*GeV*/,
   return AdlerSyst;
 }
 
-/*double CVUniverse::CalcQ2(const double Enu, const double Emu,
-                          const double Thetamu) const {
-  double Q2 =
-      2.0 * Enu *
-          (Emu - sqrt(pow(Emu, 2.0) - pow(CCNuPionIncConsts::MUON_MASS, 2.0)) *
-                     cos(Thetamu)) -
-      pow(CCNuPionIncConsts::MUON_MASS, 2.0);
-  if (Q2 < 0.) Q2 = 0.0;
-  return Q2;
-}
-*/
+//double CVUniverse::CalcQ2(const double Enu, const double Emu,
+//                          const double Thetamu) const {
+//  double Q2 =
+//      2.0 * Enu *
+//          (Emu - sqrt(pow(Emu, 2.0) - pow(CCNuPionIncConsts::MUON_MASS, 2.0)) *
+//                     cos(Thetamu)) -
+//      pow(CCNuPionIncConsts::MUON_MASS, 2.0);
+//  if (Q2 < 0.) Q2 = 0.0;
+//  return Q2;
+//}
+
 double CVUniverse::CalcQ2(const double Enu, const double Emu,
                           const double Thetamu, const double Pmu) const {
   double Q2 = 2.0*Enu*( Emu - Pmu*cos(Thetamu) ) - pow(CCNuPionIncConsts::MUON_MASS,2.0);
