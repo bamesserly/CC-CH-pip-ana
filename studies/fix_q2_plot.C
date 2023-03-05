7fb4d0a789848ec070047f3ea63aa4291ce09bb5
Ben Messerly <bamesserly@gmail.com>
2023-03-05 11:34:03 -0600

    Add xsec plotting function. Not working.

--- studies/fix_q2_plot.C
+++ studies/fix_q2_plot.C
@@ -57,6 +57,167 @@ void PlotTH1_1(TH1* h, std::string tag, double ymax = -1, bool do_log_scale = fa
   delete h1;
 }
 
+
+void Plot_CrossSection(EventSelectionPlotInfo p, MnvH1D* data, MnvH1D* mc,
+                       std::string outdir = ".", double ymax = -1,
+                       bool do_log_scale = false,
+                       bool do_bin_width_norm = true) {
+  std::cout << "Plotting CrossSection " << p.m_variable->Name() << std::endl;
+
+  // Make sure we remembered to load the source histos from the input file.
+  assert(data);
+  assert(mc);
+
+  PlotUtils::MnvH1D* data_xsec = (PlotUtils::MnvH1D*)data->Clone("data");
+  PlotUtils::MnvH1D* mc_xsec = (PlotUtils::MnvH1D*)mc->Clone("mc");
+
+  TCanvas canvas("c1", "c1");
+
+  // Get Hists
+  TH1D* data_xsec_w_tot_error = new TH1D(data_xsec->GetCVHistoWithError());
+  TH1D* data_xsec_w_stat_error = new TH1D(data_xsec->GetCVHistoWithStatError());
+  TH1D* mc_xsec_w_stat_error = new TH1D(mc_xsec->GetCVHistoWithStatError());
+
+  // Log Scale
+  if (do_log_scale) {
+    canvas.SetLogy();
+    p.m_mnv_plotter.axis_minimum = 1;
+  }
+
+  // Y-axis range
+  if (ymax > 0) p.m_mnv_plotter.axis_maximum = ymax;
+
+  // Y-label
+  p.m_mnv_plotter.axis_title_offset_y = 1.5;
+
+  // X label
+  p.SetXLabel(data_xsec_w_tot_error);
+  p.SetXLabel(data_xsec_w_stat_error);
+  p.SetXLabel(mc_xsec_w_stat_error);
+
+  // Overall Normalization
+  double pot_scale = -99.;
+  if (p.m_do_cov_area_norm) {
+    pot_scale = 1.;
+  } else {
+    pot_scale = 1.;
+  }
+
+  // Bin Width Normalization, Y-axis label, and 10^-42 shift
+  if (do_bin_width_norm) {
+    // data_xsec_w_tot_error ->Scale(1.e38, "width");
+    // data_xsec_w_stat_error->Scale(1.e38, "width");
+    // mc_xsec_w_stat_error  ->Scale(1.e38, "width");
+
+    data_xsec_w_tot_error->Scale(1.e42, "width");
+    data_xsec_w_stat_error->Scale(1.e42, "width");
+    mc_xsec_w_stat_error->Scale(1.e42, "width");
+
+    // Y label
+    // std::string yaxis = "d#sigma/d" + p.m_variable->m_hists.m_xlabel + "
+    // (10^{-38} cm^{2}/" + p.m_variable->m_units + "/nucleon)";
+    std::string yaxis = "d#sigma/d" + p.m_variable->m_hists.m_xlabel +
+                        " (10^{-42} cm^{2}/" + p.m_variable->m_units +
+                        "/nucleon)";
+    p.m_mnv_plotter.axis_title_size_y = 0.04;
+    mc_xsec_w_stat_error->GetYaxis()->SetTitle(yaxis.c_str());
+  }
+
+  /*
+  // Print xsec and error for each bin (AFTER BWN)
+  int low_edge = -99;
+  int up_edge = -99;
+  double val = -99.;
+  double err = -99.;
+  double frac_err = -99.;
+  for (int i = 0; i <= data_xsec_w_tot_error->GetNbinsX(); ++i ){
+    low_edge = data_xsec_w_tot_error->GetXaxis()->GetBinLowEdge(i);
+    up_edge  = data_xsec_w_tot_error->GetXaxis()->GetBinUpEdge(i);
+    val      = data_xsec_w_tot_error->GetBinContent(i);
+    err      = data_xsec_w_tot_error->GetBinError(i);
+    frac_err = err/val;
+
+    std::cout << i << "  " << low_edge << "  " << up_edge << "  " << val << "  "
+  << err << "  " << frac_err << "\n";
+  }
+  */
+
+  // Draw
+  const bool use_hist_titles = false;
+  p.m_mnv_plotter.DrawDataMCWithErrorBand(data_xsec_w_tot_error,
+                                          mc_xsec_w_stat_error, pot_scale, "TR",
+                                          use_hist_titles);
+
+  // Add chi2 label
+  {
+    const bool use_data_error_mtx = true;
+    const bool use_only_shape_errors = false;
+    const bool use_model_stat =
+        false;  // model statistical errors -- these are small if you use a
+                // priori effden, but atm I'm not. So keep this off.
+    // p.m_mnv_plotter.AddChi2Label(data_xsec, mc_xsec, pot_scale, "TR", 0.03,
+    // -0.175, use_data_error_mtx, use_only_shape_errors); // this auto turns on
+    // model stat errors, I've manually turned it off within the function.
+
+    // std::cout << "use_overflow is " << p.m_mnv_plotter.chi2_use_overflow_err
+    // << "\n";
+    // Check chi2
+    int ndf = -1;
+    // double pot_scale = 1.;
+    double chi2 = p.m_mnv_plotter.Chi2DataMC(
+        data_xsec, mc_xsec, ndf, pot_scale, use_data_error_mtx,
+        use_only_shape_errors, use_model_stat);
+
+    // std::cout << p.m_variable->Name() << "\n";
+    // std::cout << "   chi2 = "         << chi2     << "\n";
+    // std::cout << "   ndf = "          << ndf      << "\n";
+    // std::cout << "   chi2/ndf = "     << chi2/ndf << "\n";
+
+    // add label manually
+    if (p.m_variable->Name() == "tpi") ndf = 6;
+    if (p.m_variable->Name() == "enu") ndf = 6;
+    if (p.m_variable->Name() == "pzmu") ndf = 9;
+    if (p.m_variable->Name() == "pmu") ndf = 8;
+    if (p.m_variable->Name() == "wexp") ndf = 4;
+    char* words = Form("#chi^{2}/ndf = %3.2f/%d = %3.2f", chi2, ndf,
+                       chi2 / (Double_t)ndf);
+    int align = 33;
+    p.m_mnv_plotter.AddPlotLabel(words, 0.8, 0.745, 0.03, 1, 62, align);
+  }
+
+  // POT info
+  // -1 --> don't do mc POT
+  if (p.m_do_cov_area_norm)
+    p.m_mnv_plotter.AddAreaNormBox(p.m_data_pot, -1, 0.3, 0.88);
+  else
+    p.m_mnv_plotter.AddPOTNormBox(p.m_data_pot, -1, 0.3, 0.88);
+
+  // Label re: error bars
+  p.m_mnv_plotter.AddPlotLabel("MC stat-only errors", 0.7, 0.772, 0.03);
+
+  // Change max number of y-axis digits
+  // std::cout << "Old max digits = " << TGaxis::GetMaxDigits() << "\n";
+  // TGaxis::SetMaxDigits(4);
+  // std::cout << "New max digits = " << TGaxis::GetMaxDigits() << "\n";
+
+  // Plot Title
+  // minerva plots don't use titles
+  // p.m_mnv_plotter.title_size = 0.05;
+  // p.SetTitle("Cross Section " + GetSignalName(p.m_signal_definition));
+
+  // Print .png
+  std::string logy_str = do_log_scale ? "_logscale" : "";
+  std::string bwn_str = do_bin_width_norm ? "_BWN" : "";
+
+  std::string outfile_name =
+      Form("%s/CrossSection_%s_%s_%s%s%s", outdir.c_str(),
+           p.m_variable->Name().c_str(), p.m_do_cov_area_norm_str.c_str(),
+           GetSignalFileTag(p.m_signal_definition).c_str(), logy_str.c_str(),
+           bwn_str.c_str());
+
+  p.m_mnv_plotter.MultiPrint(&canvas, outfile_name, "png");
+}
+
 void fix_q2_plot(){
   TFile fin("/minerva/app/users/bmesserl/MATAna/cc-ch-pip-ana/DataXSecInputs_2023-02-21.root", "READ");
 
