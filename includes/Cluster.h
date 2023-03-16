cc7527eea1140027e9e07a9193ebc51de8b89a73
Ben Messerly <bamesserly@gmail.com>
2023-03-16 14:00:05 -0500

    Cluster bugfix.
    The CTOR argument order was wrong.

--- includes/Cluster.h
+++ includes/Cluster.h
@@ -2,19 +2,18 @@
 #define CLUSTER_H
 
 struct Cluster {
-  Cluster(double energy, double time, double zpos, int view, double pos);
-  Cluster(){};
   double energy;  // MeV
   double time;    // microsec
   int view;       // 1 = x, 2 = u, 3 = v
   double zpos;    // mm
   double pos;     // mm
-  int cluster_idx;
-  bool is_quality;
+  uint idx;
+  Cluster(double energy, double time, int view, double zpos, double pos, uint idx);
+  Cluster(){};
+  //bool is_quality;
 };
 
-Cluster::Cluster(double _energy, double _time, double _zpos, int _view,
-                 double _pos)
-    : energy(_energy), time(_time), view(_view), zpos(_zpos), pos(_pos) {}
+Cluster::Cluster(double _energy, double _time, int _view, double _zpos, double _pos, uint _idx)
+    : energy(_energy), time(_time), view(_view), zpos(_zpos), pos(_pos), idx(_idx) {}
 
 #endif  // CLUSTER_H
